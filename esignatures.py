d6b9e92616ac9c62770ebd292508e9cc45470a99
Apoorva Singh <apoorvasingh157@gmail.com>
2020-12-10 16:16:09 +0530

    List of templates API endpoint complete

--- esignatures.py
+++ esignatures.py
@@ -1,7 +1,32 @@
 import requests
+from requests.auth import HTTPBasicAuth
+from loguru import logger
 
-class ESginatures:
+class ESignaturesError(Exception):
     pass
 
+class ESginatures:
+    
+    def __init__(self, api_secret):
+        print(f"API Secret -> {api_secret}")
+
+        self.api_secret = api_secret
+        self.base_url = f"https://{api_secret}:@esignatures.io/api/"
+
+
+    def list_templates(self):
+
+        url = self.base_url+"templates/"
+        logger.debug(f"Target URL for fetching list of templates -> {url}")
+
+        r = requests.get(url, headers={'Content-type': 'application/json'})
+        if r.status_code == requests.codes.ok:
+            return r.json()["data"]
+
+        else:
+            logger.error("Unable to get list of templates.")
+            logger.error(f"Status code: {r.status_code}")
+            logger.error(f"Error message: {r.text}")
+            raise ESignaturesError(f"Unable to get the list of templates. Status Code: {r.status_code}")
 
 
