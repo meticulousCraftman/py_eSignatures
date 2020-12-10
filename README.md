71ba5f1a508ff2b2cf094c51bc54b88ed23d350a
Apoorva Singh <apoorvasingh157@gmail.com>
2020-12-10 16:26:22 +0530

    query_template endpoint created

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#py_eSignatures
+# py_eSignatures
 
 A library for making use of esignatures.io's API.
--- esignatures.py
+++ esignatures.py
@@ -2,31 +2,49 @@ import requests
 from requests.auth import HTTPBasicAuth
 from loguru import logger
 
+
 class ESignaturesError(Exception):
     pass
 
 class ESginatures:
     
     def __init__(self, api_secret):
-        print(f"API Secret -> {api_secret}")
-
         self.api_secret = api_secret
         self.base_url = f"https://{api_secret}:@esignatures.io/api/"
 
 
     def list_templates(self):
+        """
+        Returns the list of templates you can send.
+        """
 
         url = self.base_url+"templates/"
-        logger.debug(f"Target URL for fetching list of templates -> {url}")
 
         r = requests.get(url, headers={'Content-type': 'application/json'})
         if r.status_code == requests.codes.ok:
+            logger.success("")
             return r.json()["data"]
 
         else:
-            logger.error("Unable to get list of templates.")
-            logger.error(f"Status code: {r.status_code}")
+            logger.error(f"Unable to get list of templates. Status code: {r.status_code}")
             logger.error(f"Error message: {r.text}")
-            raise ESignaturesError(f"Unable to get the list of templates. Status Code: {r.status_code}")
 
+            raise ESignaturesError(f"Unable to get the list of templates. Status Code: {r.status_code}")
+    
+
+    def query_template(self, template_id):
+        """
+        Returns the details of the template. The Placeholder keys are specified with the template editor between {{ }} double curly brackets.
+        """
+        url = self.base_url + f"templates/{template_id}"
+        response = requests.get(url)
+
+        if response.status_code == requests.codes.ok:
+            return response.json()["data"]
+        
+        else:
+            logger.error(f"Unable to get list of templates. Status code: {r.status_code}")
+            logger.error(f"Error message: {r.text}")
+
+            raise ESignaturesError(f"Unable to query template. Status Code: {r.status_code}")
 
